33c7a931976a3a5dda450d75e3ac23fd996f3d9c
KarolineKS <kkohlsstangherlin@gmail.com>
2024-06-26 13:23:56 -0300

    arrumando variavel port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
